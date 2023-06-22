7a12e25587f3ac39855928802c6858632f62d193
inggarguitno <inggar.kinanthi.ft22@mail.umy.ac.id>
2023-06-22 17:20:42 +0700

    include iostream

--- CircularQueues_198/CircularQueues_198.cpp
+++ CircularQueues_198/CircularQueues_198.cpp
@@ -1,4 +1,26 @@
 #include <iostream>
 using namespace std;
 
-line 
+class queues {
+	int FRONT, REAR, max = 5;
+	int queues_array[5]; 
+
+public:
+	queues() {
+		FRONT = -1;
+		REAR = -1;
+	}
+
+	void insert() {
+		int num;
+		cout << "Enter a number : ";
+		cin >> num;
+		cout << endl;
+
+		//cek bila antrian penuh
+		if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
+			cout << "\nqueues overflow\n";
+			return;
+		}
+	}
+};
