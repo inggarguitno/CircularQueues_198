87779f5502c19d0af820d6950003d8d38a479f37
inggarguitno <inggar.kinanthi.ft22@mail.umy.ac.id>
2023-06-23 11:32:11 +0700

    position

--- CircularQueues_198/CircularQueues_198.cpp
+++ CircularQueues_198/CircularQueues_198.cpp
@@ -75,7 +75,7 @@ public:
 		//jika FRON position <= REAR_position. interasi FRONT hingga REAR
 		if (FRONT_position <= REAR_position) {
 			while (FRONT_position <= REAR_position) {
-				cout << queue_array[FRONT_position] << "  ";
+				cout << queue_array[FRONT_position] << "  "; 
 				FRONT_position++;
 			}
 			cout << endl;
