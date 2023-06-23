d0c9c01197906580c3688c7e02bb76287c089b8d
inggarguitno <inggar.kinanthi.ft22@mail.umy.ac.id>
2023-06-23 10:54:02 +0700

    class queue

--- CircularQueues_198/CircularQueues_198.cpp
+++ CircularQueues_198/CircularQueues_198.cpp
@@ -3,11 +3,11 @@ using namespace std;
 
 class queues {
 	int FRONT, REAR, max = 5;
-	int queues_array[5]; 
+	int queue_array[5];
 
 public:
 	queues() {
-		FRONT = -1;
+		FRONT = -1; 
 		REAR = -1;
 	}
 
@@ -22,5 +22,43 @@ public:
 			cout << "\nqueues overflow\n";
 			return;
 		}
+
+		//cek pakah antrian kosong
+		if (FRONT == -1) {
+			FRONT = 0;
+			REAR = 0;
+		}
+		else {
+			//jika REAR berada diposisi terakhir array, kembali ke awal array
+			if (REAR == max - 1)
+				REAR = 0;
+			else
+				REAR = REAR + 1;
+		}
+		queue_array(REAR) = num;
 	}
-};
+
+	void remove() {
+		//cek apakah antrin kosong
+		if (FRONT == -1) {
+			cout << " Queue underflow\n";
+			return;
+		}
+		cout << "\nThe element delected from the queue is : " << queue_array[FRONT] << "\n";
+
+		//cek jika antrian memiliki satu elemen
+		if (FRONT == REAR) {
+			FRONT = -1;
+			REAR = -1;
+		}
+		else {
+			//jika elemen dihapus berada di posisi terakhir array, kembali ke awal array
+			if (FRONT == max - 1)
+				FRONT = 0;
+			else
+				FRONT = FRONT + 1;
+		}
+	}
+}
+	
+
