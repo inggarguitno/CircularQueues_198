535abf9fdcd2946502011e472f2dd45494bbedcd
inggarguitno <inggar.kinanthi.ft22@mail.umy.ac.id>
2023-06-23 11:29:14 +0700

    void insert

--- CircularQueues_198/CircularQueues_198.cpp
+++ CircularQueues_198/CircularQueues_198.cpp
@@ -7,14 +7,14 @@ class queues {
 
 public:
 	queues() {
-		FRONT = -1;  
+		FRONT = -1;
 		REAR = -1;
 	}
 
 	void insert() {
 		int num;
 		cout << "Enter a number : ";
-		cin >> num;
+		cin >> num; 
 		cout << endl;
 
 		//cek bila antrian penuh
@@ -59,6 +59,88 @@ public:
 				FRONT = FRONT + 1;
 		}
 	}
+
+	void display() {
+		int FRONT_position = FRONT;
+		int REAR_position = REAR;
+
+		//cek apakah antrian kposong
+		if (FRONT == -1) {
+			cout << " Queue is empty\n ";
+			return;
+		}
+
+		cout << "\nElemen is the queue are...\n";
+
+		//jika FRON position <= REAR_position. interasi FRONT hingga REAR
+		if (FRONT_position <= REAR_position) {
+			while (FRONT_position <= REAR_position) {
+				cout << queue_array[FRONT_position] << "  ";
+				FRONT_position++;
+			}
+			cout << endl;
+		}
+		else {
+			//jika FRONT_position > REAR_position, interaksi dari FRONT hingga array
+			while (FRONT_position <= n - 1) {
+				cout << queue_array[FRONT_position] << "  ";
+				FRONT_position++;
+			}
+
+			FRONT_position = 0;
+			//interasi dari awal array hingga REAR
+			while (FRONT_position <= REAR_position) {
+				cout << queue_array[FRONT_position] << "  ";
+				FRONT_position++;
+			}
+			cout << endl;
+		}
+	}
+};
+
+int main() {
+	queues q;
+	char ch;
+
+	while (true) {
+		try {
+			cout << "Menu" << endl;
+			cout << "1 inplemen insert operation" << endl;
+			cout << "2 inplemen delete operation" << endl;
+			cout << "3 display values" << endl;
+			cout << "4 exit" << endl;
+			cout << "enter your choise(1-4): ";
+			cin >> ch;
+			cout << endl;
+
+			switch (ch) {
+			case '1': {
+
+				q.insert();
+				break;
+			}
+			case '2': {
+				q.remove();
+				break;
+			}
+			case '3': {
+				q.display();
+				break;
+			}
+			case '4':
+				return 0;
+			}
+			default: {
+				cout << "invalid option" << endl;
+				break;
+			}
+		}
+		catch (exception& e) {
+			cout << " Check for the values entered" << endl;
+		}
+            		    
+	}
+	return 0;
 }
 	
 
